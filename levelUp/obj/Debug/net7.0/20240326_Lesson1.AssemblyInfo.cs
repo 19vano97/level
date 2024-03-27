fff512ae94e72898e518f20f7c19307bd61fbcb5
19vano97 <vanillin97@gmail.com>
2024-03-27 17:00:16 +0200

    lesson 3. Additional files

--- levelUp/obj/Debug/net7.0/20240326_Lesson1.AssemblyInfo.cs
+++ levelUp/obj/Debug/net7.0/20240326_Lesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("20240326_Lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf555f42f22fb1247b0a2cc0c3a55d5ecef6fd96")]
 [assembly: System.Reflection.AssemblyProductAttribute("20240326_Lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("20240326_Lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
