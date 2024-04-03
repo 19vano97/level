0ef4822712a76334ca8f88b51840e1cd444ad36f
19vano97 <vanillin97@gmail.com>
2024-04-03 16:22:13 +0300

    moved dir lesson1, hw1 to root

--- levelUp/obj/Debug/net7.0/20240326_Lesson1.AssemblyInfo.cs
+++ levelUp/obj/Debug/net7.0/20240326_Lesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("20240326_Lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc390308305f8e762f36f30b91a900ffb9aa2823")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18dd323336d115ec2dc11af4c80aea4b61adaca")]
 [assembly: System.Reflection.AssemblyProductAttribute("20240326_Lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("20240326_Lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
